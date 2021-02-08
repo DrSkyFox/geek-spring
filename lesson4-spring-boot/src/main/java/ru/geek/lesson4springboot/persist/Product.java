1ef7520c763b73e49cbaee270deb277dafaeed6e
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2021-02-08 04:06:59 +0300

    lesson4 transfer to spring boot

---
+++ lesson4-spring-boot/src/main/java/ru/geek/lesson4springboot/persist/Product.java
@@ -0,0 +1,61 @@
+package ru.geek.lesson4springboot.persist;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+public class Product {
+
+    private Long id;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String description;
+
+    @NotNull
+    @Digits(integer = 6,fraction = 2)
+    @Positive
+    private Double cost;
+
+    public Product(String name, String description, Double cost) {
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+    }
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+}
